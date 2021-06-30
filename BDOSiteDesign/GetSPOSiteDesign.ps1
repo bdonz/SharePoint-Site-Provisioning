17c4aed9e868bee960accec912f40ebb3ce24ead
ViralBDONZ <84363913+ViralBDONZ@users.noreply.github.com>
2021-06-30 14:28:11 +1200

    removed-spositedesign

--- BDOSiteDesign/GetSPOSiteDesign.ps1
+++ BDOSiteDesign/GetSPOSiteDesign.ps1
@@ -1,4 +1,5 @@
 #Get-SPOSiteDesign
+Connect-SPOService 
 Get-SPOSiteDesign
 # Id                  : ddc50711-b041-4558-81c2-5115238851a2
 # Title               : BDO Communication sites with branding Demo
@@ -13,3 +14,30 @@ Get-SPOSiteDesign
 # DesignPackageId     : 00000000-0000-0000-0000-000000000000
 # DesignType          : Site
 
+
+# 30/06/2021 last run
+# Id                  : ddc50711-b041-4558-81c2-5115238851a2      
+# Title               : BDO Communication sites with branding Demo
+# WebTemplate         : 68
+# SiteScriptIds       : {d6ac6662-f5b9-4f55-9483-31f1416dbaa8}
+# Description         :
+# ThumbnailUrl        :
+# PreviewImageUrl     :
+# PreviewImageAltText :
+# IsDefault           : False
+# Version             : 1
+# DesignPackageId     : 00000000-0000-0000-0000-000000000000
+# DesignType          : Site
+
+# Id                  : 9263cdf3-a344-4b7c-a31d-fd1532e5e364
+# Title               : BDO Communication sites with branding Demo
+# WebTemplate         : 68
+# SiteScriptIds       : {8770e7ca-bf28-4a30-a381-d76b0c2060d6}
+# Description         :
+# ThumbnailUrl        :
+# PreviewImageUrl     :
+# PreviewImageAltText :
+# IsDefault           : False
+# Version             : 1
+# DesignPackageId     : 00000000-0000-0000-0000-000000000000
+# DesignType          : Site
